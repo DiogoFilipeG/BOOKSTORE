cb6f5bb77241fbe89093b040c52862760210a915
Diogo Guinapo <diogo.filipe.guinapo@gmail.com>
2024-11-15 21:35:03 +0000

    feat(GFIE-000): upgrade angular to v18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { BookList } from './models/book.model';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  private readonly http = inject(HttpClient);
+
+  books: BookList;
+
+  ngOnInit(): void {
+    this.http.get('/api/v1/books').subscribe((response: any) => {
+      this.books = response;
+      console.log(this.books);
+    });
+  }
+}
