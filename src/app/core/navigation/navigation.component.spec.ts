b5fa2aee6aa4bd9c4076f94a8778d33c6d172c89
Diogo Guinapo <diogo.filipe.guinapo@gmail.com>
2024-11-18 12:13:53 +0000

    feat(GFIE-000):  navigation component unit tests

--- src/app/core/navigation/navigation.component.spec.ts
+++ src/app/core/navigation/navigation.component.spec.ts
@@ -1,25 +1,51 @@
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MockBuilder, MockRender, ngMocks } from 'ng-mocks';
 import { NavigationComponent } from './navigation.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { of } from 'rxjs';
 
 describe('NavigationComponent', () => {
-  let component: NavigationComponent;
-  let fixture: ComponentFixture<NavigationComponent>;
-
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      imports: [NoopAnimationsModule]
-    }).compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NavigationComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(async () => {
+    await MockBuilder(NavigationComponent)
+      .mock(MatToolbarModule)
+      .mock(MatButtonModule)
+      .mock(MatSidenavModule)
+      .mock(MatListModule)
+      .mock(MatIconModule)
+      .provide({
+        provide: BreakpointObserver,
+        useValue: {
+          observe: jest.fn().mockReturnValue(of({ matches: true })),
+        },
+      });
   });
 
-  it('should compile', () => {
+  it('should create', () => {
+    const fixture = MockRender(NavigationComponent);
+    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
+  });
+
+  it('should render toolbar', () => {
+    const fixture = MockRender(NavigationComponent);
+    const toolbar = ngMocks.reveal(fixture, 'mat-toolbar');
+    expect(toolbar).toBeTruthy();
+  });
+  it('should render sidenav', () => {
+    const fixture = MockRender(NavigationComponent);
+    const sidenav = ngMocks.reveal(fixture, 'mat-sidenav');
+    expect(sidenav).toBeTruthy();
+  });
+
+  it('should emit true when handset breakpoint is matched', () => {
+    const fixture = MockRender(NavigationComponent);
+    const component = fixture.componentInstance;
+    component.isHandset$.subscribe((matches) => {
+      expect(matches).toBe(true);
+    });
   });
 });
