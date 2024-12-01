e611e040dbc92b6e66dba0d91ec0ec41405ee960
Diogo Guinapo <diogo.filipe.guinapo@gmail.com>
2024-12-01 21:22:45 +0000

    bookstore

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: `<h2 class="m-3">WELCOME TO BYND BOOKSTORE</h2> `,
+  template: `<h2 class="m-3">WELCOME TO BOOKSTORE</h2> `,
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
